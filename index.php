558aee2bb823057e89838a136e0ed8c221d012f0
Faisal-123 <fa.ok.1@hotmail.com>
2016-09-08 16:36:43 +0300

    First Commit

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Faisal H. Hijazi</title>
+        <meta name='keywords' content='Web Develpment, Web Design, تصميم مواقع , تطوير مواقع , وردبرس , Wordpress , HTML , CSS , JAVASCRIPT, '>
+        <meta name='description' content='Front End - Back End Develper , Wordpress Develper'>
+        <meta name='subject' content='Cv, Resume, for hire , official Website'>
+        <meta name='copyright' content='Faisal hijazi , فيصل حجازي'>
+        <meta name='language' content='EN,AR'>
+        <meta name='topic' content='Wordpress Development, Laravel , Web Develepment,PHP'>
+        <meta name='summary' content='موقع شخصي مختص بتطوير الويب و بناء موقع من الصفر من تصميم و برمجة او علي منصة الوردبرس'>
+        <meta name='author' content='Faisal hijazi, faisal2hijazi@gmail.com'>
+        <meta name='reply-to' content='faisal2hijazi@gmail.com'>
+        <meta name='owner' content='Self owner, Faisal hijazi فيصل حجازي'>
+        <meta name='url' content='http://www.websiteaddrress.com'>
+        <link rel="stylesheet" href="layout/css/bootstrap.min.css">
+        <link rel="stylesheet" href="layout/css/style.css">
+        <link rel="stylesheet" href="layout/css/font-awesome.min.css">
+        <link rel="stylesheet" href="layout/fonts/Palatino/Palatino.css">
+        <link href="https://fonts.googleapis.com/css?family=Rajdhani" rel="stylesheet">
+    </head>
+<body>
+  <div class="container">
+    <div class="cv">
+      <div class="my-img img-circle" id="my-img">
+        <img src="layout/img/faisal.png" alt=""  class="img-responsive img-circle"/>
+      </div>
+      <div class="about" id="about">
+        <button  class="close closeAbout" id="colseAbout">&times;</button>
+        <div class="panel">
+          <div class="panel-heading">
+            <h1 class="title"><span>About</span> Faisal</h1>
+          </div>
+          <div class="panel-body">
+            <img src="layout/img/img.jpg" alt="Faisal hijazi" class="img-responsive" />
+            <p>
+              Hi, My name is Faisal , I'm Front-End &#38; Back-End Developer from palestine, where i build HTML Templates, Web Development Using PHP &#38; PHP Framework etc.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="resume" id="resume">
+          <button  class="close closeResume" id="closeResume">&times;</button>
+          <div class="panel">
+            <div class="panel-heading">
+                <h1 class="title"><span>Resume</span></h1>
+            </div>
+            <div class="panel-body">
+              <h3>&nbsp;<i class="fa fa-graduation-cap"></i> Education</h3>
+              <div class="row edu">
+                <div class="col-xs-5">
+                  <p>High school - scientific <sup> 2009 - 2011 </sup></p>
+                  <p>Bachelor of Science <sup> 2011 - 2016 </sup></p>
+                </div>
+                <div class="col-xs-7">
+                  <p>Kamal Edwan High School</p>
+                  <p>University of Palestine <br> Faculty of Information Technology</p>
+                </div>
+              </div>
+              <h3>&nbsp;<i class="fa fa-clock-o"></i> Experience</h3>
+              <div class="row exp">
+                <div class="col-xs-5">
+                  <p> Freelancer <sup> 2016 - present </sup></p>
+                  <p> Front/Back-end Dev. <sup> 2015 - 2016 </sup>
+                </div>
+                <div class="col-xs-7">
+                  <p> Wordpress Developer &#38; Website, UI/UX Designer </p>
+                  <span>
+                    Expert In Web Development &#38; Web Designs, Skilled In PHP Web Services &#38; MYSQL Databases.
+                    And i have worked in several PHP Framworks &#38; CMS , Also Skilled In HTML5, CSS3, BOOTSTRAP &#38; Javascript
+                  </span>
+                  <p> At University of Palestine </p>
+                </div>
+              </div>
+              <h3>&nbsp;<i class="fa fa-flask"></i> Programming Skills</h3>
+              <div class="row skills">
+                  <div class="col-xs-4">
+                    <p>html5/css3/bootstrap</p>
+                    <p>javascript/ajax/jquery</p>
+                    <p>php/mysql/mvc/oop</p>
+                    <p>wordpress theme</p>
+                    <p>laravel 5 </p>
+                  </div>
+                  <div class="col-xs-8">
+                    <div class="progress">
+                         <div class="progress-bar progress-bar-danger" role="progressbar" area-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width:90%"></div>
+                    </div>
+                    <div class="progress">
+                         <div class="progress-bar progress-bar-danger" role="progressbar" area-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width:75%"></div>
+                    </div>
+                    <div class="progress">
+                         <div class="progress-bar progress-bar-danger" role="progressbar" area-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:80%"></div>
+                    </div>
+                    <div class="progress">
+                         <div class="progress-bar progress-bar-danger" role="progressbar" area-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%"></div>
+                    </div>
+                    <div class="progress">
+                         <div class="progress-bar progress-bar-danger" role="progressbar" area-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:50%"></div>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+      </div>
+      <div class="portfolio" id="portfolio">
+        <button class="close closePortfolio" id="closePortofolio">&times;</button>
+        <!-- portfolio 1 -->
+        <div class="panel-image">
+          <div class="caption">
+            <h4>The Voice Of university of Palestine</h4>
+            <p>short thumbnail description</p>
+            <p><a href="" class="label label-danger">Zoom</a>
+            <a href="" class="label label-default">Download</a></p>
+          </div>
+          <img src="layout/img/up.jpg" alt="" class="img-responsive" />
+        </div>
+        <!-- portfolio 2 -->
+        <div class="panel-image">
+          <div class="caption">
+            <h4>Thumbnail Headline</h4>
+            <p>short thumbnail description</p>
+            <p><a href="" class="label label-danger">Zoom</a>
+            <a href="" class="label label-default">Download</a></p>
+          </div>
+          <img src="layout/img/img.jpg" alt="" class="img-responsive" />
+        </div>
+        <!-- portfolio 3 -->
+        <div class="panel-image">
+          <div class="caption">
+            <h4>Thumbnail Headline</h4>
+            <p>short thumbnail description</p>
+            <p><a href="" class="label label-danger">Zoom</a>
+            <a href="" class="label label-default">Download</a></p>
+          </div>
+          <img src="layout/img/img.jpg" alt="" class="img-responsive" />
+        </div>
+      </div>
+      <div class="coverLerter">
+          <h1> Faisal H. Hijazi </h1>
+          <p> Front-end/Back-end Developer </p>
+      </div>
+
+      <div class="contact">
+        <ul class="list-unstyled list-inline">
+          <li class="aboutIcon" data-toggle="tooltip" data-placement="top" title="About"><a class="aboutIcon"><i class="fa fa-user"></i></a></li>
+          <li class="resumeIcon" data-toggle="tooltip" data-placement="top" title="Resume"><a class="resumeIcon"><i class="fa fa-file-o"></i></a></li>
+          <li class="portfolioIcon" data-toggle="tooltip" data-placement="top" title="Portfolio"><a class="portfolioIcon"><i class="fa fa-briefcase"></i></a></li>
+          <li data-container="body" data-toggle="popover" data-placement="bottom" data-content="Contact Me At :  faisal2hijazi@gmail.com" ><a data-toggle="tooltip" data-placement="top" title="Email"><i class="fa fa-envelope-o"></i></a></li>
+          <li data-toggle="tooltip" data-placement="top" title="Facebook"><a href="https://www.facebook.com/faksha12" target="_blank"><i class="fa fa-facebook"></i></a></li>
+          <li data-toggle="tooltip" data-placement="top" title="Codepen"><a href="https://codepen.io/JFaisal" target="_blank"><i class="fa fa-codepen"></i></a></li>
+          <li data-toggle="tooltip" data-placement="top" title="Github"><a href="https://www.facebook.com/faksha12" target="_blank"><i class="fa fa-github"></i></a></li>
+          <li data-toggle="tooltip" data-placement="top" title="Behance"><a href="https://www.behance.net/fahij" target="_blank"><i class="fa fa-behance"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  
+    <script src="layout/js/jquery-1.12.0.min.js"></script>
+    <script src="layout/js/jquery.nicescroll.min.js"></script>
+    <script src="layout/js/bootstrap.min.js"></script>
+    <script src="layout/js/backend.js"></script>
+</body>
+</html>
